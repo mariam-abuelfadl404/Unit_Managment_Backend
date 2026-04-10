895d0f9e9de9ee7a6d49d904f44f535e398fb147
mariam-abuelfadl404 <ma1758392@gmail.com>
2026-04-10 21:12:56 +0200

    add changes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // handle preflight لكل الـ routes
+// app.options(); // handle preflight لكل الـ routes
 
 // ── Security & Parsing ────────────────────────────────────
 app.use(helmet({ crossOriginResourcePolicy: false }));
